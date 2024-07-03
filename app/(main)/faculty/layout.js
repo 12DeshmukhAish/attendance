8716568b497eae0c1630b00767a3e12be8d29dfb
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2024-07-03 19:16:14 +0530

    Structure created

---
+++ app/(main)/faculty/layout.js
@@ -0,0 +1,20 @@
+import Sidebar from "@/app/components/facultySidebar"
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+export default async function RootLayout({ children }) {
+  const session = await getServerSession(authOptions);
+  const role = session?.user?.role
+ 
+  if ((role==="teaching") || !session) {
+    console.log("unauthorised")
+    redirect("/login");
+   }
+  return (<div className="flex">
+    <Sidebar/>
+    <div className="w-full h-screen overflow-y-auto" >
+        {children}
+      </div> 
+      </div>
+  );
+}
